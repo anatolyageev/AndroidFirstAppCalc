badb1ab255c67c1b28213bdb6dd165e57b8b103d
Anatolii Ageev <anatol1812@gmail.com>
2021-04-15 20:31:39 +0300

    added simple calculator

--- app/src/main/java/com/example/androidfirstappcalc/Logic.java
+++ app/src/main/java/com/example/androidfirstappcalc/Logic.java
@@ -1,8 +1,36 @@
 package com.example.androidfirstappcalc;
 
 import java.math.BigDecimal;
-import java.util.BitSet;
+import java.math.MathContext;
 
 public class Logic {
-    private BigDecimal a;
+    private static final int PRECISION = 5;
+    public static final MathContext MATH_CONTEXT = new MathContext(PRECISION);
+
+
+    public static String executeOperation(String op1, String op2, String operation) {
+        BigDecimal result = new BigDecimal("0.0", MATH_CONTEXT);
+        BigDecimal bdOp1 = new BigDecimal(op1, MATH_CONTEXT);
+        BigDecimal bdOp2 = new BigDecimal(op2, MATH_CONTEXT);
+
+        switch(operation) {
+            case "-":
+                result = bdOp1.subtract(bdOp2, MATH_CONTEXT);
+                break;
+            case "+":
+                result = bdOp1.add(bdOp2, MATH_CONTEXT);
+                break;
+            case "/":
+                if (bdOp2.equals(new BigDecimal(0))) {
+                    result = new BigDecimal(0);
+                }else {
+                    result = bdOp1.divide(bdOp2, MATH_CONTEXT);
+                }
+                break;
+            case "*":
+                result = bdOp1.multiply(bdOp2, MATH_CONTEXT);
+                break;
+        }
+        return result.toString();
+    }
 }
--- app/src/main/java/com/example/androidfirstappcalc/MainActivity.java
+++ app/src/main/java/com/example/androidfirstappcalc/MainActivity.java
@@ -1,45 +1,117 @@
 package com.example.androidfirstappcalc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
+    private String previousNumber;
+    private String operation;
+    private boolean isNewDigit = true;
+    public final static String TAG_INFO = "Info message ==> ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /**Получение компонентов из лайаута
-         *
-         */
-//        TextView textView = findViewById(R.id.text1);
-//        Button button1 = findViewById(R.id.button);
-        //
-//        button1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                textView.setText("First input");
-//                System.out.println("Console hi");
-//                Log.e("Log e -->", "Some f message");
-//                Log.v("Log e -->", "Some f message");
-//            }
-//        });
         editText = findViewById(R.id.editText);
-
-
+        editText.setText("0");
     }
 
+    public void backSpaceBtn(View view) {
+        String number = editText.getText().toString();
+        if (!number.equals("0")) {
+            number = number.substring(0, number.length() - 1);
+        }
+        editText.setText(number);
+    }
+
+    public void clearAll(View view) {
+        editText.setText("0");
+        previousNumber = null;
+        operation = null;
+        isNewDigit = true;
+    }
+
+    public void clearNumber(View view) {
+        editText.setText("0");
+        operation = null;
+        isNewDigit = true;
+    }
+
+    @SuppressLint("SetTextI18n")
     public void numHandler(View view) {
-        Button button = (Button) view;
-        editText.append(button.getText());
+        Log.i(TAG_INFO, "previousNumber : " + previousNumber);
+        Log.i(TAG_INFO, "operation : " + operation);
+        if (isNewDigit) {
+            editText.setText("0");
+            isNewDigit = false;
+        }
+
+        Button btn = (Button) view;
+        String number = editText.getText().toString();
+        String buttonText = btn.getText().toString();
+
+        if (Character.isDigit(buttonText.charAt(0))) {
+            if (!number.equals("0")) {
+                editText.append(buttonText);
+            } else {
+                editText.setText(buttonText);
+            }
+        } else {
+            switch (buttonText) {
+                case "+/-":
+                    if (!number.equals("0")) {
+                        if (number.charAt(0) == '-') {
+                            editText.setText(number.substring(1));
+                        } else {
+                            editText.setText("-" + number);
+
+                        }
+                        break;
+                    }
+
+                case "-":
+                    signResolveHelper(number, "-");
+                    break;
+                case "+":
+                    signResolveHelper(number, "+");
+                    break;
+                case "/":
+                    signResolveHelper(number, "/");
+                    break;
+                case "*":
+                    signResolveHelper(number, "*");
+                    break;
+
+                case "=":
+                    if (previousNumber == null) {
+                        editText.setText("0");
+                    } else {
+                        editText.setText(Logic.executeOperation(previousNumber, number, operation));
+                        previousNumber = null;
+                    }
+                    isNewDigit = true;
+                    operation = null;
+                    break;
+            }
+        }
     }
 
+    private void signResolveHelper(String number, String sign) {
+        if (previousNumber == null) {
+            previousNumber = number;
+        } else {
+            previousNumber = Logic.executeOperation(previousNumber, number, operation);
+        }
+        operation = sign;
+        editText.setText("0");
+    }
 }
